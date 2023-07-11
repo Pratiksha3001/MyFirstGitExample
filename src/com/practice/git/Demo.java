940fc04e6fcbea7bc6bb671ee781cb0b36c7a840
pratiksha <pratikshagholap77@gmail.com>
2023-07-11 22:28:55 +0530

    First Commit

---
+++ src/com/practice/git/Demo.java
@@ -0,0 +1,10 @@
+package com.practice.git;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		 System.out.println("Welcome to git program practice");
+
+	}
+
+}
---
+++ src/com/practice/git/Test.java
@@ -0,0 +1,10 @@
+package com.practice.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 System.out.println("Welcome to git Test Class");
+
+	}
+
+}
